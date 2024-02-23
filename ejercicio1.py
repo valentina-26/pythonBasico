b011059d40c960988f3a3eab813bdf44f3530318
valentina-26~ <160797346+valentina-26@users.noreply.github.com>
2024-02-23 13:34:04 -0500

    ejercicio2

--- ejercicio1.py
+++ ejercicio1.py
@@ -1,4 +1,12 @@
-num1 = input("ingrese el numero 1: ")
-num2 = input("ingrese el numero 2: ")
-suma = int(num1)+int(num2)
-print(suma)
+num1=int(input("ingrese el primer numero:"))
+num2=int(input("ingrese el segundo numero:"))
+num3=int(input("ingrese el tercer numero:"))
+
+if int(num1)>int(num2) and int(num1)>int(num3):
+    print ("el primer numero es mayor")
+
+elif int(num2)>int(num1) and int(num2)>(num3):
+    print("el segundo numero es mayor")
+
+else:
+    print("el tercer numero es mayor")
