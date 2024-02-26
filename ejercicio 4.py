bf00713da7218b797e7e5553a18900ba643d52a0
valentina-26 <160797346+valentina-26@users.noreply.github.com>
2024-02-26 16:44:17 -0500

    ejercicio 6

--- ejercicio 4.py
+++
@@ -1,3 +0,0 @@
-C = float(input("ingrese la cantidad de grados celsius que desea convertir: "))
-F = (C*9/5) + 32
-print(C,"grados celsius equivale a: ",F,"grados fagrenheit")
---
+++ ejercicio 6.py
@@ -0,0 +1,6 @@
+print("ingrese el numero")
+num=int(input())
+if num % 2==0:
+    print("el numero" ,num, "es par")
+else:
+    print("el numero",num,"es impar")
