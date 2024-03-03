f9172c26e1d46540d8239a8cd91c068cb93feb61
valentina-26 <160797346+valentina-26@users.noreply.github.com>
2024-03-02 19:46:59 -0500

    promedio

---
+++ Promedio.py
@@ -0,0 +1,10 @@
+primer_nota=float(input("ingrese su primer nota: "))
+segunda_nota=float(input("ingrese su segunda nota: "))
+tercera_nota=float(input("ingrese su tercera nota: "))
+cuarta_nota=float(input("ingrese su cuarta nota: "))
+
+suma=float(primer_nota+segunda_nota+tercera_nota+cuarta_nota)
+
+promedio=float(suma/4)
+
+print ("su promedio es: " , promedio)
